507a344c3000b5968d0dd0a169409324b3a7fdfe
Spencer <Spencer@368eaded-f0bd-94f6-f52d-f1199e7d05e4>
2012-06-26 16:03:03 +0000

    Add methods for the DragonFly

--- trunk/src/com/campcomputer/entity/ChuckNorris.java
+++ trunk/src/com/campcomputer/entity/ChuckNorris.java
@@ -1,2 +1,24 @@
-public class ChuckNorris {
+package com.campcomputer.entity;
+
+import com.campcomputer.Entity;
+
+public class ChuckNorris extends Entity {
+	public void attacks(){
+
+	}
+
+	public void punches(){
+
+
+	}
+	public void roudhousekick(){
+
+
+	}
+	public void lasereyes(){
+
+	}
+	public void armor(){
+
+	}
 }
