5174a4170b7add358f2680f5741b0e0b1f1fd68d
jpitlor@gmail.com <jpitlor@gmail.com@368eaded-f0bd-94f6-f52d-f1199e7d05e4>
2013-07-12 02:42:20 +0000

    added rest of the ammo box images, added different ammo for rifle

--- src/com/campcomputer/journeyofthehairs/ammo/Ammo.java
+++ src/com/campcomputer/journeyofthehairs/ammo/Ammo.java
@@ -1,11 +1,13 @@
 package com.campcomputer.journeyofthehairs.ammo;
 
 import com.campcomputer.journeyofthehairs.GameEngine;
-import com.campcomputer.journeyofthehairs.item.Item;
+import com.campcomputer.journeyofthehairs.Images;
+import com.campcomputer.journeyofthehairs.item.*;
 
 public abstract class Ammo extends Item {
+    public Item item;
+
     public Ammo(GameEngine engine) {
         super(engine);
-
     }
 }
---
+++ src/com/campcomputer/journeyofthehairs/ammo/RifleShot.java
@@ -0,0 +1,14 @@
+package com.campcomputer.journeyofthehairs.ammo;
+
+import com.campcomputer.journeyofthehairs.GameEngine;
+import com.campcomputer.journeyofthehairs.item.Rifle;
+
+public class RifleShot extends Rifle {
+    public RifleShot (GameEngine engine) {
+        super(engine);
+    }
+
+    public void loadImages() {
+
+    }
+}
